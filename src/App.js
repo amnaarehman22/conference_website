4fa5afc33b115e8fa39aad0edba957238656d100
Amna Abdul Rehman <119008215+amnaarehman22@users.noreply.github.com>
2023-03-07 22:31:33 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
 import {CTA, Brand, Navbar} from './components';
+import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
+      <Navbar />
+      <Header />
       <div className='gradient__bg'>
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
+        <Brand />
+      </div>      
       <WhatGPT3 />
       <Features />
       <Possibility />
